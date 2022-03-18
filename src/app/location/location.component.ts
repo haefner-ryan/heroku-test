e30fe3214225c816ff4ae9f5a5f062018f9048ce
Ryan Haefner <ryan.haefner@risen-one.com>
2022-03-17 20:06:44 -0400

    _

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -7,12 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationComponent implements OnInit {
   number: number = 0;
+  lat!: number;
+  long!: number;
+
+  updatedLat!: number;
+  updatedLong!: number;
 
   constructor() { }
 
   ngOnInit(): void {
     navigator.geolocation.getCurrentPosition(res => {
       console.log(res)
+      this.lat = res.coords.latitude;
+      this.long = res.coords.longitude;
+    })
+    navigator.geolocation.watchPosition(res => {
+      console.log(res);
+      this.updatedLat = res.coords.latitude;
+      this.updatedLong = res.coords.longitude;
     })
   }
 
